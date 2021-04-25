4b21d3ef3109302650d0470c31b36510ad00ee37
ZEKER�YA DURANEL <2013010217088@ogrenci.karabuk.edu.tr>
2021-04-26 01:00:27 +0300

    2

---
+++ API.Data/DataContext/StoreContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using API.Data.DbModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data.DataContext
+{
+    public class StoreContext :DbContext
+    {
+        public StoreContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,50 @@
+﻿using API.Data.DataContext;
+using API.Data.DbModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    //https:localhost:5001/api/Products/GetProducts
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+
+        private readonly StoreContext _context;
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+
+
+
+        }
+
+        
+        [HttpGet] //http verbs -> GET POST DELETE PUT vs..
+        public async Task<ActionResult<List<Product>>> GetProducts()
+        {
+            var data = await _context.Products.ToListAsync();
+            return data;
+        }
+    
+        [HttpGet("{id}")]
+        public ActionResult<Product> GetProduct(int id)
+        {
+
+
+            return _context.Products.Find(id);
+        }
+
+        
+
+
+        
+
+    }
+}
