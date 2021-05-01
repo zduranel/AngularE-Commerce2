de05ec3327c9da5e208a35ae7568fb4905c09322
ZEKER�YA DURANEL <2013010217088@ogrenci.karabuk.edu.tr>
2021-05-02 02:18:56 +0300

    Entityle Oluşturuldu Generic Repositoryler oluşturuldu

--- API.Infrastructure/Implements/GenericRepository.cs
+++ API.Infrastructure/Implements/GenericRepository.cs
@@ -1,5 +1,7 @@
 ﻿using API.Core.DbModels;
 using API.Core.Interfaces;
+using API.Infrastructure.DataContext;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,14 +12,21 @@ namespace API.Infrastructure.Implements
 {
     public class GenericRepository<T> : IGenericRepository<T> where T : BaseEntity
     {
-        public Task<T> GetByIdAsync(int id)
+        private readonly StoreContext _context;
+        public GenericRepository(StoreContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
         }
 
-        public Task<IReadOnlyList<T>> ListAllAsync()
+
+        public async Task<T> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            return await _context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<IReadOnlyList<T>> ListAllAsync()
+        {
+            return await _context.Set<T>().ToListAsync();
         }
     }
 }
